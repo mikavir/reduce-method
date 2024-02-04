c0899712c2087a5253936c10ad0f17e756d41ac8
Mika Virtucio <mikavirtucio@gmail.com>
2024-02-04 19:16:12 +0000

    Initial commit

--- reduce.js
+++ reduce.js
@@ -6,7 +6,18 @@
 
 
 // Summing an array of numbers:
+const nums = [ 0, 1, 2, 3 ,4 ];
+// let sum = nums.reduce((acc, curr) => acc + curr);
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    "accummalator:", acc,
+    "current:", curr,
+    "total:", acc + curr,
+  );
+  return acc + curr;
+}, 10);
 
+console.log(sum);
 
 const teamMembers = [
   {
@@ -32,6 +43,29 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience , 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// {developer: 12, designer: 4} <-- what we want
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {})
+
+// grouping by names
+let nameByProfession = teamMembers.reduce((acc, cur) => {
+  let group = cur.profession;
+  if (!acc[group]) {
+    acc[group] = [];
+  }
+  acc[group].push(cur.name);
+  return acc;
+}, {})
+console.log(nameByProfession);
+
